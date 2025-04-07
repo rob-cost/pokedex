9024862c2c99afc945a0bb972d994da9e5e1b8cb
rob-cost <robertocostantino3@gmail.com>
2025-04-07 15:09:03 +0200

    Created array of objects
    creation of an array with different objects  about details of different Pokemons

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let pokemonList=[
+    {name:'Bulbasaur', hegiht:0.7, type:['grass', 'poison']},
+    {name:'Charmander', hegiht:0.6, type:'fire'},
+    {name:'Squirtle', hegiht:0.5, type:'water'}
+]
