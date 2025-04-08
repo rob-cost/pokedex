f132bbf79e587a89a09c6e46ac28a9fafb7a6b03
rob-cost <robertocostantino3@gmail.com>
2025-04-08 16:08:55 +0200

    Created a loop function to interact within the array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
+// Creation of an array with Pokemon details
+
 let pokemonList=[
-    {name:'Bulbasaur', hegiht:0.7, type:['grass', 'poison']},
-    {name:'Charmander', hegiht:0.6, type:'fire'},
-    {name:'Squirtle', hegiht:0.5, type:'water'}
-]
+    {
+     name:'Bulbasaur',
+     height:0.7, 
+     type:['grass', 'poison']
+    },
+
+    {
+     name:'Charmander',
+     height:0.6,
+     type:'fire'
+    },
+    
+    {
+     name:'Squirtle',
+     height:0.5,
+     type:'water'
+    }
+];
+
+
+for (ii = 0; ii < pokemonList.length; ii++) {               // for loop for select each obkect in the array
+
+    if (pokemonList[ii].height > 0.6) {                     // condition to add a text only to a object with soecific value
+                        
+        document.write(pokemonList[ii].name + " (height: " + pokemonList[ii].height + ") " + " - Wow, that's big" +  "<br><br>" );  // print out 
+    }
+
+    else {
+        
+        document.write(pokemonList[ii].name + " (height: " + pokemonList[ii].height + ") " + "<br><br>" );  // print out
+    }
+    
+}
