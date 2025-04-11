33e3d2f3ea21f1ee8727b342bee7e31c530c72c4
rob-cost <robertocostantino3@gmail.com>
2025-04-11 15:03:59 +0200

    Changed type of function
    used for.Each function instead of for loop to iterate with the array

--- js/script.js
+++ js/script.js
@@ -20,18 +20,9 @@ let pokemonList=[
     }
 ];
 
+// Loop which iterate with the array and print each item details
 
-    function printArrayDetails(list) {                                  // declaring a function
-        for (ii = 0; ii < list.length; ii++) {               // for loop for select each obkect in the array
-            if (list[ii].height > 0.6) {                     // condition to add a text only to a object with soecific value                   
-                document.write(` <p> ${list[ii].name} (height: ${list[ii].height}) - Wow, that's big </p>`);      // print out result
-            }
-            else {
-                document.write(` <p> ${list[ii].name} (height: ${list[ii].height}) </p>` );  // print out result
-             } 
-        }
-    }
+pokemonList.forEach ( list => {                                     
+        document.write(` <p> Name: ${list.name} <br> Height: ${list.height} <br> Type: ${list.type} </p>`);      
+})
 
-
-
-printArrayDetails(pokemonList);
