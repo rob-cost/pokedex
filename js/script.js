517ee40b4a749b5bf5f29af380cb8e52d21fb44b
rob-cost <robertocostantino3@gmail.com>
2025-04-12 11:54:03 +0200

    Improved add function

--- js/script.js
+++ js/script.js
@@ -3,20 +3,20 @@
 let pokemonRepository = (function () {
 let pokemonList=[
     {
-     name:'Bulbasaur',
-     height:0.7, 
+     name: 'Bulbasaur',
+     height: 0.7, 
      type:['grass', 'poison']
     },
 
     {
-     name:'Charmander',
-     height:0.6,
+     name: 'Charmander',
+     height: 0.6,
      type:['fire']
     },
     
     {
-     name:'Squirtle',
-     height:0.5,
+     name: 'Squirtle',
+     height: 0.5,
      type:['water']
     }
 ];
@@ -24,7 +24,12 @@ let pokemonList=[
 // Implementing two functions as keys
 
 function add(pokemon){
+    if (typeof pokemon === 'object' && Object.keys(pokemonList) === 'name', 'height', 'type') {
     pokemonList.push(pokemon);
+    }
+    else {
+        alert(`please insert the right value`)
+    };
 }
 
 function getAll(){
@@ -38,6 +43,14 @@ return {
 
 })();
 
+// Adding a new Pokemon with add function
+
+pokemonRepository.add({
+    name: 'Pikachu',
+    height: 0.5,
+    type: ['electric']
+});
+
 
 // For.each loop that iterate in the array and print each item details
 
@@ -49,4 +62,6 @@ pokemonRepository.getAll().forEach( list => {
         document.write(` <p> Name: ${list.name} <br> Height: ${list.height} <br> Type: ${list.type} </p>`); 
     }  
 });
+
+
 
